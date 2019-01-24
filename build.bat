25bfbb46788e4d2a1c7f305c6d7d1701724b54d7
Francesco_Sblendorio <francesco.sblendorio@jobrapido.com>
2019-01-24 21:41:12 +0100

    Add phonebook modify and save

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@echo off
 del /S/Q target
 md target
 
@@ -18,26 +19,21 @@ del *.inf
 
 cd ..\target
 
-c1541 -format "ultimatedemos,sh" d64 ultimatedemos.d64
-c1541 -format "ultimate-term,sh" d64 ultimate-term.d64
+c1541 -format "ultimatedemos,sh" d64 UltimateTerm-and-demos.d64
 
-c1541 -attach ultimatedemos.d64 -write ..\src\term-loader.prg term-loader
-c1541 -attach ultimatedemos.d64 -write u-shell.prg u-shell
-c1541 -attach ultimatedemos.d64 -write u-term64.prg u-term64
-c1541 -attach ultimatedemos.d64 -write u-term128.prg u-term128
-c1541 -attach ultimatedemos.d64 -write u-term.seq u-term,s
-c1541 -attach ultimatedemos.d64 -write u-chat64.prg u-chat64
-c1541 -attach ultimatedemos.d64 -write u-chat128.prg u-chat128
-c1541 -attach ultimatedemos.d64 -geoswrite geouterm.cvt
-c1541 -attach ultimatedemos.d64 -geoswrite ..\src\cbm40.cvt
-c1541 -attach ultimatedemos.d64 -geoswrite ..\src\cbm80.cvt
+c1541 -attach UltimateTerm-and-demos.d64 -write ..\src\term-loader.prg term-loader
+c1541 -attach UltimateTerm-and-demos.d64 -write u-term64.prg u-term64
+c1541 -attach UltimateTerm-and-demos.d64 -write u-term128.prg u-term128
+c1541 -attach UltimateTerm-and-demos.d64 -write u-shell.prg u-shell
+c1541 -attach UltimateTerm-and-demos.d64 -write u-chat64.prg u-chat64
+c1541 -attach UltimateTerm-and-demos.d64 -write u-chat128.prg u-chat128
+c1541 -attach UltimateTerm-and-demos.d64 -geoswrite geouterm.cvt
+c1541 -attach UltimateTerm-and-demos.d64 -geoswrite ..\src\cbm40.cvt
+c1541 -attach UltimateTerm-and-demos.d64 -geoswrite ..\src\cbm80.cvt
+c1541 -attach UltimateTerm-and-demos.d64 -write u-term.seq u-term,s
 
-c1541 -attach ultimate-term.d64 -write ..\src\term-loader.prg term-loader
-c1541 -attach ultimate-term.d64 -write u-term64.prg u-term64
-c1541 -attach ultimate-term.d64 -write u-term128.prg u-term128
-c1541 -attach ultimate-term.d64 -write u-term.seq u-term,s
-
-del u-term.seq
+del /Q u-term.seq
+del /Q geouterm.cvt
 
 cd ..
 
