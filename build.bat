3cc0caf702f78fbcd7028bab3ccf1241341db169
Francesco_Sblendorio <francesco.sblendorio@jobrapido.com>
2019-02-02 16:23:05 +0100

    #19 added u-sample to d64

--- build.bat
+++ build.bat
@@ -25,6 +25,7 @@ c1541 -attach UltimateTerm-and-demos.d64 -write ..\src\term-loader.prg term-load
 c1541 -attach UltimateTerm-and-demos.d64 -write u-term64.prg u-term64
 c1541 -attach UltimateTerm-and-demos.d64 -write u-term128.prg u-term128
 c1541 -attach UltimateTerm-and-demos.d64 -write u-shell.prg u-shell
+c1541 -attach UltimateTerm-and-demos.d64 -write u-sample.prg u-sample
 c1541 -attach UltimateTerm-and-demos.d64 -write u-chat64.prg u-chat64
 c1541 -attach UltimateTerm-and-demos.d64 -write u-chat128.prg u-chat128
 c1541 -attach UltimateTerm-and-demos.d64 -geoswrite geouterm.cvt
