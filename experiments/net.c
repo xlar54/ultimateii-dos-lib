d126d799646dd036a02bc77a5fdcdc44b6750b2b
Francesco_Sblendorio <francesco.sblendorio@jobrapido.com>
2019-02-05 20:53:04 +0100

    trying connecting http

--- experiments/net.c
+++ experiments/net.c
@@ -12,19 +12,28 @@
 unsigned char socketnr = 0;
 int datacount;
 char buff[400];
-
 void main(void) {
 	//int i;
+    long z;
+
+    for (z=0; z<3000L; z++);
+    printf("PROVA UNO\n");
+    uii_settarget(TARGET_NETWORK);
 	uii_identify();
 	uii_getipaddress();
-	uii_settarget(TARGET_NETWORK);
+
+    printf("\nIP Address: %d.%d.%d.%d", uii_data[0], uii_data[1], uii_data[2], uii_data[3]);
+    printf("\n   Netmask: %d.%d.%d.%d", uii_data[4], uii_data[5], uii_data[6], uii_data[7]);
+    printf("\n   Gateway: %d.%d.%d.%d\n", uii_data[8], uii_data[9], uii_data[10], uii_data[11]);
+    
+
 	uii_tcpconnect("cbbsoutpost.servebbs.com", 80);
 	socketnr = uii_data[0];
     if (uii_status[0] == '0' && uii_status[1] == '0') {
     	printf("FASE 1-----------------\n");
         // strcpy(buff,"GET /api/exportbbslist/service.php?f=csv HTTP/1.0\n\n");
-        strcpy(buff,"get /INDEX.PHP http/1.0\n\n");
-    	uii_tcpsocketwrite(socketnr, buff);
+        strcpy(buff,"GET / HTTP/1.0\012\012");
+    	uii_tcpsocketwrite(socketnr, "get /index.php\012\012\012\012");
 
     	printf("FASE 2-----------------\n");
         while(1) {
