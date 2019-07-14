8ef2822159300b852cf368f40f5d1761c3e40752
Scott Hutter <scott.hutter@gmail.com>
2019-07-14 10:31:20 -0500

    re-added echoserver to build after 2.01 fix

--- build.bat
+++ build.bat
@@ -8,6 +8,7 @@ cl65 -O -t c64 ..\lib\ultimate_lib.c u-sample.c -o  ..\..\target\u-sample.prg
 cl65 -O -t c64 ..\lib\ultimate_lib.c u-menu.c -o    ..\..\target\u-menu.prg
 cl65 -O -t c64 ..\lib\ultimate_lib.c u-shell.c -o   ..\..\target\u-shell.prg
 cl65 -O -t c64 ..\lib\ultimate_lib.c u-chat.c -o    ..\..\target\u-chat64.prg
+cl65 -O -t c64 ..\lib\ultimate_lib.c u-echoserver.c -o    ..\..\target\u-echoserver.prg
 cl65 -O -t c64 ..\lib\ultimate_lib.c screen_utility.c ultimateterm.c -o  ..\..\target\u-term64.prg
 cl65 -O -t c128 ..\lib\ultimate_lib.c screen_utility.c ultimateterm.c -o ..\..\target\u-term128.prg
 cl65 -O -t c128 ..\lib\ultimate_lib.c u-chat.c -o ..\..\target\u-chat128.prg
@@ -30,6 +31,7 @@ c1541 -attach UltimateTerm-and-demos.d64 -write u-sample.prg u-sample
 c1541 -attach UltimateTerm-and-demos.d64 -write u-menu.prg u-menu
 c1541 -attach UltimateTerm-and-demos.d64 -write u-chat64.prg u-chat64
 c1541 -attach UltimateTerm-and-demos.d64 -write u-chat128.prg u-chat128
+c1541 -attach UltimateTerm-and-demos.d64 -write u-echoserver.prg u-echoserver
 c1541 -attach UltimateTerm-and-demos.d64 -write geouterm.cvt geoterm.cvt
 c1541 -attach UltimateTerm-and-demos.d64 -write ..\src\samples\cbm40.cvt cbm40.cvt
 c1541 -attach UltimateTerm-and-demos.d64 -write ..\src\samples\cbm80.cvt cbm80.cvt
